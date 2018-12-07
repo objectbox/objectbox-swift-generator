4472b6162f2fa7b0304da7864b939e220966598c
Uli <noreply+uli@objectbox.io>
2018-12-07 15:49:15 +0100

    Wrote a first test that just compares output files.

--- ObjectBoxToolTests/RunToolTests.sh
+++ ObjectBoxToolTests/RunToolTests.sh
@@ -11,10 +11,27 @@ cd ${BUILT_PRODUCTS_DIR}
 
 SOURCERY="${BUILT_PRODUCTS_DIR}/Sourcery.app/Contents/MacOS/Sourcery"
 TESTPROJECT="${PROJECT_DIR}/ObjectBoxToolTests/ToolTestProject.xcodeproj"
-TESTMODELFILE="${BUILT_PRODUCTS_DIR}/model1.json"
-OUTPUTFILE="${BUILT_PRODUCTS_DIR}/"
+TESTMODELFILE="${BUILT_PRODUCTS_DIR}/model.json"
+OUTPUTFILE="${BUILT_PRODUCTS_DIR}/EntityInfo.generated.swift"
 
-$SOURCERY --xcode-project "$TESTPROJECT" --xcode-target ToolTestProject --model-json "$TESTMODELFILE" --output "$OUTPUTFILE"
+ORIGINALMODELFILE="${PROJECT_DIR}/ObjectBoxToolTests/model1.json"
+ORIGINALSOURCEFILE="${PROJECT_DIR}/ObjectBoxToolTests/EntityInfo.generated1.swift"
+cp "${PROJECT_DIR}/ObjectBoxToolTests/model1.json" "$TESTMODELFILE"
+
+$SOURCERY --xcode-project "$TESTPROJECT" --xcode-target ToolTestProject --model-json "$TESTMODELFILE" --output "`dirname '$OUTPUTFILE'`"
+cmp --silent "$TESTMODELFILE" "$ORIGINALMODELFILE"
+if [ $? -eq 0 ]; then
+    echo "note: Model files match."
+else
+    echo "error: Model files DIFFERENT!"
+fi
+
+cmp --silent "$OUTPUTFILE" "$ORIGINALSOURCEFILE"
+if [ $? -eq 0 ]; then
+echo "note: Output files match."
+else
+echo "error: Output files DIFFERENT!"
+fi
 
 rm "$TESTMODELFILE"
 rm "$OUTPUTFILE"
