1198e0ac8758149f514bb87caf9a7bc588d4cde9
Uli <noreply+uli@objectbox.io>
2018-12-07 16:08:47 +0100

    Shell script test harness now done. Now all we need to do is add model and output files, and source files, and add them to the test project.

--- ObjectBoxToolTests/RunToolTests.sh
+++ ObjectBoxToolTests/RunToolTests.sh
@@ -14,26 +14,49 @@ TESTPROJECT="${PROJECT_DIR}/ObjectBoxToolTests/ToolTestProject.xcodeproj"
 TESTMODELFILE="${BUILT_PRODUCTS_DIR}/model.json"
 OUTPUTFILE="${BUILT_PRODUCTS_DIR}/EntityInfo.generated.swift"
 
-ORIGINALMODELFILE="${PROJECT_DIR}/ObjectBoxToolTests/model1.json"
-ORIGINALSOURCEFILE="${PROJECT_DIR}/ObjectBoxToolTests/EntityInfo.generated1.swift"
-cp "${PROJECT_DIR}/ObjectBoxToolTests/model1.json" "$TESTMODELFILE"
+test_target_num () {
+    FAIL=0
 
-$SOURCERY --xcode-project "$TESTPROJECT" --xcode-target ToolTestProject --model-json "$TESTMODELFILE" --output "`dirname '$OUTPUTFILE'`"
-cmp --silent "$TESTMODELFILE" "$ORIGINALMODELFILE"
-if [ $? -eq 0 ]; then
-    echo "note: Model files match."
-else
-    echo "error: Model files DIFFERENT!"
-fi
+    ORIGINALMODELFILE="${PROJECT_DIR}/ObjectBoxToolTests/model${3}.json"
+    cp "$ORIGINALMODELFILE" "$TESTMODELFILE"
 
-cmp --silent "$OUTPUTFILE" "$ORIGINALSOURCEFILE"
-if [ $? -eq 0 ]; then
-echo "note: Output files match."
-else
-echo "error: Output files DIFFERENT!"
-fi
+    $SOURCERY --xcode-project "$TESTPROJECT" --xcode-target "$2" --model-json "$TESTMODELFILE" --output "`dirname '$OUTPUTFILE'`"
 
-rm "$TESTMODELFILE"
-rm "$OUTPUTFILE"
+    cmp --silent "$TESTMODELFILE" "$ORIGINALMODELFILE"
+    if [ $? -eq 0 ]; then
+        echo "note: $1: Model files match."
+    else
+        echo "error: $1: Model files DIFFERENT!"
+
+        echo "===== test: ====="
+        cat "$TESTMODELFILE"
+        echo "===== original: ====="
+        cat "$ORIGINALMODELFILE"
+        echo "====="
+        FAIL=1
+    fi
+
+    ORIGINALSOURCEFILE="${PROJECT_DIR}/ObjectBoxToolTests/EntityInfo.generated${3}.swift"
+    cmp --silent "$OUTPUTFILE" "$ORIGINALSOURCEFILE"
+    if [ $? -eq 0 ]; then
+        echo "note: $1: Output files match."
+    else
+        echo "error: $1: Output files DIFFERENT!"
+
+        echo "===== test: ====="
+        cat "$OUTPUTFILE"
+        echo "===== original: ====="
+        cat "$ORIGINALSOURCEFILE"
+        echo "====="
+        FAIL=1
+    fi
+
+    rm "$TESTMODELFILE"
+    rm "$OUTPUTFILE"
+
+    exit $FAIL
+}
+
+test_target_num "Simple Model" ToolTestProject 1
 
 echo "note: Finished tests..."
